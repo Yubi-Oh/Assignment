5882188d384fd872cebb206113ba9f0e8b69d98f
Yubi-Oh <eu1451@gmail.com>
2024-04-16 20:52:35 +0900

    FileO 1 and 2

---
+++ FileO12.py
@@ -0,0 +1,19 @@
+Python 3.13.0a4 (tags/v3.13.0a4:9d34f60, Feb 15 2024, 17:00:30) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #1 다음 보기를 입력과 관련된 함수와 출력과 관련된 함수로 분류하세요.
+>>> ## 입력: input(), read(), readline(), readlines()
+>>> ## 출력: print(0, write(), writeline()
+>>> 
+>>> #2 다음은 파일의 처리단계입니다. 순서대로 나열한 것이 맞는 것을 고르세요.
+>>> ## 정답: 1번 A-->C-->B-->D
+>>> 
+>>> #3 파일의 열기 모드에 대한 설명입니다. 거리가 먼 것을 모두 고르세요.
+>>> ## 정답: 1번, 2번
+>>> 
+>>> #4 다음은 data1.txt 파일에서 1개 행만 읽어서 출력하는 코드입니다. 빈칸을 채우세요.
+>>> ## 정답: 1) open 2) readline 3) close
+>>> 
+>>> #6 다음은 파일을 복사하는 코드입니다. 빈칸을 채우세요.
+>>> 
+>>> ## 정답 1) readlines() 2) write()
+>>> 
